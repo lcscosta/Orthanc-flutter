2fb91a94fcd7bcaefef4011b0cf8eabfd38dbf11
lcscosta <lcscosta@disroot.org>
2023-06-01 17:23:30 -0300

    Added Menu Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -196,6 +196,60 @@ class _HomePageState extends State<HomePage> {
           );
         },
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Menu',
+                  style: TextStyle(color: Colors.white, fontSize: 24)),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              leading: const Icon(Icons.home),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Settings'),
+              leading: const Icon(Icons.settings),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            const AboutListTile(
+              // <-- SEE HERE
+              icon: Icon(
+                Icons.info,
+              ),
+              applicationIcon: Icon(
+                Icons.local_play,
+              ),
+              applicationName: 'Orthanc Flutter',
+              applicationVersion: '1.0.0',
+              applicationLegalese: '© 2023 lcscosta',
+              aboutBoxChildren: [
+                ///Content goes here...
+              ],
+              child: Text('About app'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
